b9afaf0f80a1e86132522e2e19e500d392aa57a9
Huan LI <zixia@zixia.net>
2018-04-26 12:24:58 +0800

    fix typo in filename

--- src/clone-class.ts
+++ src/clone-class.ts
@@ -7,16 +7,18 @@ export const VERSION = require('../package.json').version as string
 
 // https://github.com/Microsoft/TypeScript/issues/10262
 // https://github.com/Microsoft/TypeScript/pull/13743
+// https://github.com/Microsoft/TypeScript/issues/5843#issuecomment-290972055
 export type Constructor<T> = new(...args: any[]) => T
 
 // tslint:disable-next-line:variable-name
 export function cloneClass<T extends Constructor<{}>>(OrignalClass: T): T {
+
   class NewClass extends OrignalClass {
     constructor(...args: any[]) {
       super(...arguments)
     }
   }
-  return NewClass as any as T
+  return NewClass
 }
 
 export default cloneClass
