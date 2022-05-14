47f8788f27e5a59293453f1c94f6a4e949bc8551
Huan (李卓桓) <zixia@zixia.net>
2022-05-14 21:59:45 +0800

    add unit tests

--- src/instance-to-class.spec.ts
+++ src/instance-to-class.spec.ts
@@ -3,6 +3,7 @@
 import { test } from 'tstest'
 
 import { FixtureClass }     from '../tests/fixtures/fixture-class.js'
+import { cloneClass } from './clone-class.js'
 
 import { instanceToClass }  from './instance-to-class.js'
 
@@ -10,9 +11,21 @@ test('instanceToClass smoke testing', async t => {
   const instance = new FixtureClass(1, 2)
   const SameFixtureClass = instanceToClass(instance, FixtureClass)
   t.equal(SameFixtureClass, FixtureClass, 'should get back the same Class for its own instance')
+})
 
+test('instanceToClass work with extended class', async t => {
   class ChildFixtureClass extends FixtureClass {}
   const anotherInstance = new ChildFixtureClass(3, 4)
-  const AnotherFixtureClass = instanceToClass(anotherInstance, FixtureClass)
-  t.not(AnotherFixtureClass, FixtureClass, 'should get back another Class for instance from its child class')
+  const SameChildFixtureClass = instanceToClass(anotherInstance, FixtureClass)
+  t.equal(SameChildFixtureClass, ChildFixtureClass, 'should get back ChildFixtureClass for instance from its child class')
+})
+
+test('instanceToClass work with cloneClass', async t => {
+  const ChildFixtureClass = cloneClass(FixtureClass)
+  ;(ChildFixtureClass as any).n = 42
+  const anotherInstance = new ChildFixtureClass(3, 4)
+  const SameChildFixtureClass = instanceToClass(anotherInstance, FixtureClass)
+  t.equal(SameChildFixtureClass, ChildFixtureClass, 'should get back ChildFixtureClass for instance from its child class')
+  t.equal((SameChildFixtureClass as any).n, 42, 'should get exactly the class with 42')
+  t.equal((FixtureClass as any).n, undefined, 'should has not affect the original class')
 })
